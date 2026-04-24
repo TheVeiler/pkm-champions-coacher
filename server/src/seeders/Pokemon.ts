581fb7fafefd47516f4c1ce1ce2e46fcf0e9f909
TheVeiler <duboisvalentin1010@gmail.com>
2026-04-24 02:20:43 +0200

    added info into Pokemon seed

--- server/src/seeders/Pokemon.ts
+++ server/src/seeders/Pokemon.ts
@@ -5,7 +5,33 @@ import jsonPokemons from "./Pokemon.json" with { type: "json" };
 export default {
 	up: async () => {
 		try {
-			await Pokemon.bulkCreate(jsonPokemons);
+			await Pokemon.bulkCreate(jsonPokemons.map(({
+                no,
+                name,
+                name_en,
+                image,
+                base_stats,
+                real_stats
+            }) => ({
+                no,
+                name,
+                name_en,
+                image,
+
+                base_hp: base_stats.hp,
+                base_atk: base_stats.atk,
+                base_def: base_stats.def,
+                base_spatk: base_stats.spatk,
+                base_spdef: base_stats.spdef,
+                base_spe: base_stats.spe,
+
+                real_hp: real_stats.hp,
+                real_atk: real_stats.atk,
+                real_def: real_stats.def,
+                real_spatk: real_stats.spatk,
+                real_spdef: real_stats.spdef,
+                real_spe: real_stats.spe,
+            })));
 		} catch (e) {
 			console.error(e);
 		}
