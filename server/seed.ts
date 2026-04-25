f6a2be901601e659f70e3b76516f559a13b5f899
TheVeiler <duboisvalentin1010@gmail.com>
2026-04-25 12:35:38 +0200

    added seeder for Move

--- server/seed.ts
+++ server/seed.ts
@@ -1,8 +1,8 @@
-import { Ability, Item, Nature, Pokemon, Type } from "./src/seeders/index.js";
+import { Ability, Item, Move, Nature, Pokemon, Type } from "./src/seeders/index.js";
 
 await Ability.up();
 await Item.up();
-// await Move.up();
+await Move.up();
 await Nature.up();
 await Pokemon.up();
 await Type.up();
